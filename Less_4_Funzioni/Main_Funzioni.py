58c13e08a7375f3818ad7eba23a8253718dc5002
AndreaDagg <daguannoandrea@outlook.com>
2020-01-04 15:27:41 +0100

    function2

--- Less_4_Funzioni/Main_Funzioni.py
+++ Less_4_Funzioni/Main_Funzioni.py
@@ -18,3 +18,38 @@ def funzione_con_valore_default(lista, supermercato="Conad"):
 
 
 funzione_con_valore_default(["carne", "caciocavallo", "acqua"])
+
+
+# Programmazione ad Oggetti
+# definiamo la classe ed il metodo __init__ è il costruttore che verrà chiamato automaticamente
+# il piromo parametro di un metodo di una classe è self
+
+class Triangle:
+    """
+    Questa classe rappresenta un tirangolo: descrivo la classe
+    """
+
+    def __init__(self, a, b, c, h):
+        self.a, self.b, self.c, self.h = a, b, c, h
+
+    def area(self):
+        return self.b * self.h / 2
+
+    def perimeter(self):
+        return self.a + self.b + self.c
+
+    # Metodo che utilizzaa altri metodi
+    def print_inf(self):
+        print(F"Area {self.area()}")
+        print(F"Perimetro {self.perimeter()}")
+
+
+triangle = Triangle(3, 5, 6, 7)
+# triangle.area(3, 5)
+# triangle.perimeter(3, 5, 8)
+triangle.perimeter()
+triangle.area()
+triangle.print_inf()
+
+#ritorna i metodi presnti in una classe
+help(triangle)
